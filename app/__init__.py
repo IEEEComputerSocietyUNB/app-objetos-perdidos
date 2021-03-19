ef99d58a0322348d31b87a4001c4d0b79e59b96e
Joao Victor Rodrigues da Silva <victor.joao.1611@gmail.com>
2021-03-19 13:29:47 +0000

    Create and Read objects in database

--- app/__init__.py
+++ app/__init__.py
@@ -1,33 +1,58 @@
 import os
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 import mysql.connector
+from .model import Object, get_objects, add_object
+from datetime import date
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
+    from . import database
+    database.init_app(app)
+
     @app.route('/')
     def hello():
         return render_template('index.html')
-    
-    @app.route('/db_test')
-    def db():
-        db = mysql.connector.connect(
-            host="localhost", 
-            user="dev", 
-            password="password"
-        )
 
+
+    @app.route('/objects', methods=['GET', 'POST'])
+    def objects():
+        db = database.get_db()
         cursor = db.cursor()
 
-        result = cursor.execute("SHOW DATABASES")
+        if request.method == 'GET':
+            query = get_objects.format()
+            cursor.execute(query)
 
-        result = ""
-        for register in cursor:
-            result += str(register)
+            objs = []
+            for register in cursor:
+                objs.append(Object._make(register))
 
-        return str(result)
+            database.close_db()
+            
+            return render_template('objects.html', objs=objs)
 
+        else:
+            name = request.form['name']
+            description = request.form['description']
+            create_date = str(date.today())
+            query = add_object.format(name=name, description=description, create_date=create_date)
+
+            cursor.execute(query)
+            db.commit()
+
+            database.close_db()
+
+            return redirect(url_for('objects'))
+
+    
+
+    @app.route('/db_test')
+    def db_test():
+        database.init_db()
+
+        return 'Banco de dados inicializado com sucesso!' 
 
 
     return app
