be03faf083b226165b32bbcbb1a6a6573217843a
Ingrid Lorraine Rodrigues da Silva <ingridsilva71@gmail.com>
2021-03-27 12:02:42 -0300

    Integrate views

--- app/__init__.py
+++ app/__init__.py
@@ -11,31 +11,22 @@ def create_app(test_config=None):
     from . import database
     database.init_app(app)
 
+
     @app.route('/')
     def hello():
-        return render_template('index.html')
+        # database.init_db()
+        return redirect(url_for('view_objects'))
+    
 
-
-    @app.route('/objects', methods=['GET', 'POST'])
-    def objects():
-        db = database.get_db()
-        cursor = db.cursor()
-
+    @app.route('/objects/create', methods=['GET', 'POST'])
+    def create_object():
         if request.method == 'GET':
-            query = get_objects.format()
-            cursor.execute(query)
-
-            registers = cursor.fetchall()
-
-            objs = []
-            for register in registers:
-                objs.append(Object._make(register))
-
-            database.close_db()
-            
-            return render_template('objects.html', objs=objs)
+            return render_template('registerObject/index.html')
 
         else:
+            db = database.get_db()
+            cursor = db.cursor()
+
             name = request.form['name']
             description = request.form['description']
             create_date = date.today().strftime('%Y-%m-%d')
@@ -49,33 +40,98 @@ def create_app(test_config=None):
             return redirect(url_for('objects'))
 
 
-
-    @app.route('/objects/<int:id>', methods=['PUT', 'DELETE'])
-    def object_update_or_delete(id):
+    @app.route('/objects/view', methods=['GET'])
+    def view_objects():
         db = database.get_db()
         cursor = db.cursor()
 
-        if request.method == 'DELETE':
-            query = delete_object.format(id=id)
-            cursor.execute(query)
-            db.commit()
+        query = get_objects.format()
+        cursor.execute(query)
 
-            database.close_db()
+        registers = cursor.fetchall()
 
-            return redirect(url_for('objects'), code=303)
-        else:
-            name = request.form['name']
-            description = request.form['description']
+        objs = []
+        for register in registers:
+            objs.append(Object._make(register))
 
-            create_date = date.today().strftime('%Y-%m-%d')
+        database.close_db()
+        
+        return render_template('showCase/index.html', objs=objs)
 
-            query = update_object.format(id=id, name=name, description=description, create_date=create_date)
-            cursor.execute(query)
-            db.commit()
 
-            database.close_db()
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
 
-            return redirect(url_for('objects'), code=303)
+        return render_template('login/index.html')
+
+
+    @app.route('/signup', methods=['GET', 'POST'])
+    def signup():
+
+        return render_template('signUp/index.html')
+        
+
+    @app.route('/objects', methods=['GET', 'POST'])
+    def objects():
+        return redirect(url_for('view_objects'))
+        # db = database.get_db()
+        # cursor = db.cursor()
+
+        # if request.method == 'GET':
+        #     query = get_objects.format()
+        #     cursor.execute(query)
+
+        #     registers = cursor.fetchall()
+
+        #     objs = []
+        #     for register in registers:
+        #         objs.append(Object._make(register))
+
+        #     database.close_db()
+            
+        #     return render_template('objects.html', objs=objs)
+
+        # else:
+        #     name = request.form['name']
+        #     description = request.form['description']
+        #     create_date = date.today().strftime('%Y-%m-%d')
+        #     query = add_object.format(name=name, description=description, create_date=create_date)
+
+        #     cursor.execute(query)
+        #     db.commit()
+
+        #     database.close_db()
+
+        #     return redirect(url_for('objects'))
+
+
+
+    # @app.route('/objects/<int:id>', methods=['PUT', 'DELETE'])
+    # def object_update_or_delete(id):
+    #     db = database.get_db()
+    #     cursor = db.cursor()
+
+    #     if request.method == 'DELETE':
+    #         query = delete_object.format(id=id)
+    #         cursor.execute(query)
+    #         db.commit()
+
+    #         database.close_db()
+
+    #         return redirect(url_for('objects'), code=303)
+    #     else:
+    #         name = request.form['name']
+    #         description = request.form['description']
+
+    #         create_date = date.today().strftime('%Y-%m-%d')
+
+    #         query = update_object.format(id=id, name=name, description=description, create_date=create_date)
+    #         cursor.execute(query)
+    #         db.commit()
+
+    #         database.close_db()
+
+    #         return redirect(url_for('objects'), code=303)
     
 
     @app.route('/db_test')
